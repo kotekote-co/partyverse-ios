416ef4419f01e07daf8cf973650b5a127ad1ebdb
Renat Kislov <renatkaveev@gmail.com>
2023-05-06 20:41:17 +0500

    Initial Commit

---
+++ partyverse/partyverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  partyverseApp.swift
+//  partyverse
+//
+//  Created by Ренат on 06.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct partyverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
