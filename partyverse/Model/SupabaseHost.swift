87be24e65a4fd5395c10f2ad5f118fc38c6f7bb8
Renat Kislov <renatkaveev@gmail.com>
2023-05-21 17:10:41 +0500

    Add script for token

--- partyverse/Model/SupabaseHost.swift
+++ partyverse/Model/SupabaseHost.swift
@@ -3,4 +3,4 @@ import Supabase
 
 let url = URL(string: "https://vwkinpuayzmtdzvqoako.supabase.co")
 
-let client = SupabaseClient(supabaseURL: url!, supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3a2lucHVheXptdGR6dnFvYWtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUzNTQwMzcsImV4cCI6MTk5MDkzMDAzN30.C_ouXJu2yQ2Nuw1GhzunAnpWYbQiM4H0H3Jjij_AV8U")
+let client = SupabaseClient(supabaseURL: url!, supabaseKey: Bundle.main.object(forInfoDictionaryKey: "SupabaseAccessToken") as! String)
