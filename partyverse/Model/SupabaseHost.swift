f1b0b7090ab6c97e425c5eb453f1e6c070691387
Renat Kislov <renatkaveev@gmail.com>
2023-05-21 14:46:45 +0500

    Init supabase

---
+++ partyverse/Model/SupabaseHost.swift
@@ -0,0 +1,6 @@
+import Foundation
+import Supabase
+
+let url = URL(string: "https://vwkinpuayzmtdzvqoako.supabase.co")
+
+let client = SupabaseClient(supabaseURL: url!, supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3a2lucHVheXptdGR6dnFvYWtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUzNTQwMzcsImV4cCI6MTk5MDkzMDAzN30.C_ouXJu2yQ2Nuw1GhzunAnpWYbQiM4H0H3Jjij_AV8U")
