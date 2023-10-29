46505e5a56857332ca02c18f53469581a2656aee
Renat Kislov <keetsta@icloud.com>
2023-10-29 18:22:39 +0500

    Initial Commit

---
+++ partyverse/partyverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  partyverseApp.swift
+//  partyverse
+//
+//  Created by keet on 29.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct partyverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
